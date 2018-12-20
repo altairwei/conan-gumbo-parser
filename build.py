fa715e2c645841a314eb1665551d496a73151e43
Altair Wei <altair_wei@outlook.com>
2018-12-20 19:25:46 +0800

    create build.py

---
+++ build.py
@@ -0,0 +1,6 @@
+from cpt.packager import ConanMultiPackager
+
+if __name__ == "__main__":
+    builder = ConanMultiPackager(username="altairwei", archs=["x86_64"], build_policy="missing")
+    builder.add_common_builds(pure_c=True)
+    builder.run()
